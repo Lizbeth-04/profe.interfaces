aa6d24bb99c2186ada06f39c127e2e53d2b5dd49
Lizbeth-04 <lencalada@sudamericano.edu.ec>
2023-10-31 17:40:01 -0500

    lista

---
+++ listadecompras/compras.js
@@ -0,0 +1,8 @@
+<h1>lista de compras</h1>;
+<h2>frutas</h2>;
+<h3>manzanas verdes y rojas</h3>;
+<h4>uvas verdes y negras </h4>;
+<h5>mangos</h5>;
+<h6>duraznos</h6>;
+<h7>mandarinas</h7>;
+<h8> no se encontro las uvas verdes </h8>;
